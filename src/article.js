22f46af094b9b491e7c8f04b23e011dd1436cbe2
Frank Mckenna <fmckenna56330@gmail.com>
2021-08-04 09:40:09 +0100

    using env variables in javascript

--- src/article.js
+++ src/article.js
@@ -1,19 +1,19 @@
-  "use strict";
-  
-  class Article {
+class Article {
     static render () {
-      const url = 'https://content.guardianapis.com/search?q=football&api-key=f6150436-16fa-4d9d-aa06-5cfffdd496b9';
+      const url = `https://content.guardianapis.com/search?q=olympics&api-key=f6150436-16fa-4d9d-aa06-5cfffdd496b9`;
       fetch(url)
       .then(response => response.json())
       .then(data => {
         let stories = data.response.results;
         stories.forEach(story => {
           const p = document.createElement('p');
-          p.textContent = story.webTitle;
+          p.innerHTML = `<a href ="${story.webUrl}">${story.webTitle}</a>`;
           document.body.appendChild(p);
       })
-    }).catch(error => console.error(error))
+    }).catch(error => console.log(error))
   }
 }
-
 Article.render();
+
+
+
